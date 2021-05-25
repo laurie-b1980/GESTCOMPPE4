53a4592864b0282cde5bd0fc01f892af5ad1ebbd
Laurie <laurie.delfosse59@gmail.com>
2021-05-25 10:13:04 +0200

    test barre de recherche

--- lib/Screens/clients2.dart
+++ lib/Screens/clients2.dart
@@ -1,6 +1,7 @@
-/*import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:testflutter/Screens/AjoutClient.dart';
+
 import 'package:testflutter/services/database.dart';
 import 'package:testflutter/Screens/detailClient.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -18,66 +19,7 @@ class _ClientsState extends State<Clients> {
     return await db.recupClients();
   }
 
-  TextEditingController _searchcontroller = TextEditingController();
-
-  Future resultsLoaded;
-  List _allResults = [];
-  List _resultsList = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _searchcontroller.addListener(_onSearchChanged);
-  }
-
-  @override
-  void dispose() {
-    _searchcontroller.removeListener(_onSearchChanged);
-    _searchcontroller.dispose();
-    super.dispose();
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    resultsLoaded = rechercheClientStreamSnapshot();
-  }
-
-  _onSearchChanged() {
-    searchResultList();
-    print(_searchcontroller.text);
-  }
-
-  searchResultList() {
-    var showResults = [];
-    if (_searchcontroller.text != "") {
-      for (var clientsnapshot in _allResults) {
-        var title = rechercheClientStreamSnapshot().title.toLowerCase();
-        if (title.contains(_searchcontroller.text.toLowerCase())) {
-          showResults.add(clientsnapshot);
-        }
-      }
-    } else {
-      showResults = List.from(_allResults);
-    }
-
-    setState(() {
-      _resultsList = _allResults;
-    });
-  }
-
-  rechercheClientStreamSnapshot() async {
-    var data = await FirebaseFirestore.instance
-        .collection('clients')
-        .where('nom', isEqualTo: _searchcontroller.text)
-        .orderBy('nom')
-        .get();
-    setState(() {
-      _allResults = data.docs;
-    });
-    searchResultList();
-    return "Complet";
-  }
+  TextEditingController _textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -89,7 +31,7 @@ class _ClientsState extends State<Clients> {
           }
           if (snapshot.connectionState == ConnectionState.done) {
             print("message: ${snapshot.data.docs}");
-            print("message ");
+
             return Scaffold(
               appBar: AppBar(
                 title: Text('GESTCOM clients'),
@@ -111,7 +53,7 @@ class _ClientsState extends State<Clients> {
                 child: ListView(
                     padding: const EdgeInsets.only(left: 70.0),
                     children: [
-                      Column(children: [
+                      Row(children: [
                         TextButton(
                           onPressed: () {
                             Navigator.push(
@@ -122,24 +64,14 @@ class _ClientsState extends State<Clients> {
                           },
                           child: Text('Ajouter'),
                         ),
-                        TextField(
-                            controller: _searchcontroller,
-                            decoration: InputDecoration(
-                                prefixIcon: Icon(Icons.search))),
-                        Expanded(
-                            child: ListView.builder(
-                                itemCount: _resultsList.length,
-                                itemBuilder:
-                                    (BuildContext context, int index) =>
-                                        _resultsList[index])),
-                        /*TextButton(
+                        TextButton(
                             onPressed: () {
                               /*showSearch(
                                   context: context,
                                   delegate: RechercherItem(widget.listExample));
-                              print('$listExample');
-                            },*/
-                            child: Text('Rechercher'))*/
+                              print(listExample);*/
+                            },
+                            child: Text('Rechercher')),
                       ]),
                       Container(
                           padding: EdgeInsets.symmetric(vertical: 5.0),
@@ -172,7 +104,7 @@ class _ClientsState extends State<Clients> {
           return Text('Chargement...');
         });
   }
-}*/
+}
 
 /*AjoutCommandes(
                                   idclient: idclient,
